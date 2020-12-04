07dcf691b0759403d6969c9e6cdbe73636761fdd
mryamz <mryamz@users.noreply.github.com>
2020-12-03 17:18:06 -0800

    Press Space!

---
+++ Assets/DismissableScreenManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DismissableScreenManager : MonoBehaviour {
+
+    public Button[] tabs;
+    public Displaceable[] contents;
+    public Displaceable mainContent;
+
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            mainContent.isValid = !mainContent.isValid;
+        }
+
+    }
+
+    public void DisableContents() {
+        foreach (Displaceable d in contents) {
+            d.isValid = false;
+        }
+    }
+
+    public void OnTabPressed(int activeIndex) {
+        DisableContents();
+        contents[activeIndex].isValid = true;
+    }
+}
