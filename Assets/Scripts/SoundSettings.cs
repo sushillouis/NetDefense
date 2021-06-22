ace838d5408a0874acf95b1d79064f085e3c17cf
joshuadahlunr <joshuadahl@nevada.unr.edu>
2021-06-22 16:23:29 -0700

    Add Sound Settings.
    Added another icon at the bottom of the screen which can be used to change the sound volume settings.

---
+++ Assets/Scripts/SoundSettings.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class SoundSettings : MonoBehaviour {
+	public GameObject settingsPannel;
+	public Slider musicVolume, soundFXVolume;
+	public AudioMixer masterMixer;
+
+	void Start(){
+		masterMixer.GetFloat("musicVol", out float mvol);
+		masterMixer.GetFloat("sfxVol", out float svol);
+
+		musicVolume.value = mvol;
+		soundFXVolume.value = svol;
+	}
+
+	public void ToggleSettings(){
+		Debug.Log(settingsPannel.activeSelf);
+		if(settingsPannel.activeSelf)
+			settingsPannel.SetActive(false);
+		else
+			settingsPannel.SetActive(true);
+	}
+
+	public void MusicVolumeChanged(){
+		masterMixer.SetFloat("musicVol", musicVolume.value);
+	}
+
+	public void SoundFXVolumeChanged(){
+		masterMixer.SetFloat("sfxVol", soundFXVolume.value);
+	}
+
+}
