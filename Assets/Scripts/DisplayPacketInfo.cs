9a28b0cbe6ee110a43e57c36fdf945b0afec24ff
atozc <catoz@nevada.unr.edu>
2021-10-12 00:32:59 -0700

    Added text to packet UI

---
+++ Assets/Scripts/DisplayPacketInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayPacketInfo : MonoBehaviour
+{
+    public static DisplayPacketInfo inst;
+    private void Awake()
+    {
+        inst = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject HoverInfo = new GameObject("text");  
+    }
+
+    public Text color;               
+    public Text shape;
+    public Text size;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //color.text = 
+        //shape.text = SelectionManager.Instantiate.selected.name;
+        //size.text = SelectionManager.Instantiate.selected.name;
+    }
+}
+ 
