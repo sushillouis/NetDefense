29d787d7fe73bc506d054755bdec07349d8b8526
atozc <catoz@nevada.unr.edu>
2021-10-15 00:38:03 -0700

    Updated script & UI

--- Assets/Scripts/DisplayPacketInfo.cs
+++ Assets/Scripts/DisplayPacketInfo.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class DisplayPacketInfo : MonoBehaviour
 {
     public static DisplayPacketInfo inst;
+    public GameObject SimpleEnemyController; 
+
     private void Awake()
     {
         inst = this;
@@ -21,13 +23,51 @@ public class DisplayPacketInfo : MonoBehaviour
     public Text shape;
     public Text size;
 
-
     // Update is called once per frame
     void Update()
     {
-        //color.text = 
-        //shape.text = SelectionManager.Instantiate.selected.name;
-        //size.text = SelectionManager.Instantiate.selected.name;
+        //Color Labels 
+        if (SimpleEnemyController.GetComponent<SimpleEnemyController>().color == 0)
+        {
+            color.text = SimpleEnemyController.GetComponent<SimpleEnemyController>().color.ToString("Pink");
+        }
+        if (SimpleEnemyController.GetComponent<SimpleEnemyController>().color == 1)
+        {
+            color.text = SimpleEnemyController.GetComponent<SimpleEnemyController>().color.ToString("Green");
+        }
+        if (SimpleEnemyController.GetComponent<SimpleEnemyController>().color == 2)
+        {
+            color.text = SimpleEnemyController.GetComponent<SimpleEnemyController>().color.ToString("blue");
+        }
+
+        //Size Labels
+        if (SimpleEnemyController.GetComponent<SimpleEnemyController>().size == 0)
+        {
+            size.text = SimpleEnemyController.GetComponent<SimpleEnemyController>().size.ToString("S");
+        }
+        if (SimpleEnemyController.GetComponent<SimpleEnemyController>().size == 1)
+        {
+            size.text = SimpleEnemyController.GetComponent<SimpleEnemyController>().size.ToString("M");
+        }
+        if (SimpleEnemyController.GetComponent<SimpleEnemyController>().size == 2)
+        {
+            size.text = SimpleEnemyController.GetComponent<SimpleEnemyController>().size.ToString("L");
+        }
+
+        //Shape Labels 
+        if (SimpleEnemyController.GetComponent<SimpleEnemyController>().shape == 0)
+        {
+            shape.text = SimpleEnemyController.GetComponent<SimpleEnemyController>().shape.ToString("Cube");
+        }
+        if (SimpleEnemyController.GetComponent<SimpleEnemyController>().shape == 1)
+        {
+            shape.text = SimpleEnemyController.GetComponent<SimpleEnemyController>().shape.ToString("Cone");
+        }
+        if (SimpleEnemyController.GetComponent<SimpleEnemyController>().shape == 2)
+        {
+            shape.text = SimpleEnemyController.GetComponent<SimpleEnemyController>().shape.ToString("Sphere");
+        }
+
     }
 }
  
