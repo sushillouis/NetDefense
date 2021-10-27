8570afe4dd9e16dc23e099a10f9ac11d56c521ae
atozc <catoz@nevada.unr.edu>
2021-10-27 15:00:32 -0700

    Added Assets

---
+++ Assets/RotateItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateItem : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, 0.5f, 0f));
+    }
+}
